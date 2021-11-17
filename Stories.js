b6219f6670b8789fadce260697093e72b5e02765
Shreyash <shreyashchaple786oji@gmail.com>
2021-11-17 17:39:27 +0530

    Stories files uploaded

---
+++ Stories.js
@@ -0,0 +1,144 @@
+function readmore1(){
+    var dots= document.getElementById("Dots1");
+    var moretext= document.getElementById("more1");
+    var btn= document.getElementById("btn1");
+
+    if(dots.style.display == "none"){
+        dots.style.display== "inline";
+        btn.innerHTML = "Read more";
+        moretext.style.display = "none";
+    }
+    else{
+        dots.style.display = "none";
+        btn.innerHTML = "Read less";
+        moretext.style.display = "inline";
+    }
+}
+function readmore2(){
+  var dots= document.getElementById("Dots2");
+  var moretext= document.getElementById("more2");
+  var btn= document.getElementById("btn2");
+
+  if(dots.style.display == "none"){
+      dots.style.display== "inline";
+      btn.innerHTML = "Read more";
+      moretext.style.display = "none";
+  }
+  else{
+      dots.style.display = "none";
+      btn.innerHTML = "Read less";
+      moretext.style.display = "inline";
+  }
+}
+function readmore3(){
+  var dots= document.getElementById("Dots3");
+  var moretext= document.getElementById("more3");
+  var btn= document.getElementById("btn3");
+
+  if(dots.style.display == "none"){
+      dots.style.display== "inline";
+      btn.innerHTML = "Read more";
+      moretext.style.display = "none";
+  }
+  else{
+      dots.style.display = "none";
+      btn.innerHTML = "Read less";
+      moretext.style.display = "inline";
+  }
+}
+function readmore4(){
+  var dots= document.getElementById("Dots4");
+  var moretext= document.getElementById("more4");
+  var btn= document.getElementById("btn4");
+
+  if(dots.style.display == "none"){
+      dots.style.display== "inline";
+      btn.innerHTML = "Read more";
+      moretext.style.display = "none";
+  }
+  else{
+      dots.style.display = "none";
+      btn.innerHTML = "Read less";
+      moretext.style.display = "inline";
+  }
+}
+function readmore5(){
+  var dots= document.getElementById("Dots5");
+  var moretext= document.getElementById("more5");
+  var btn= document.getElementById("btn5");
+
+  if(dots.style.display == "none"){
+      dots.style.display== "inline";
+      btn.innerHTML = "Read more";
+      moretext.style.display = "none";
+  }
+  else{
+      dots.style.display = "none";
+      btn.innerHTML = "Read less";
+      moretext.style.display = "inline";
+  }
+}
+function readmore6(){
+  var dots= document.getElementById("Dots6");
+  var moretext= document.getElementById("more6");
+  var btn= document.getElementById("btn6");
+
+  if(dots.style.display == "none"){
+      dots.style.display== "inline";
+      btn.innerHTML = "Read more";
+      moretext.style.display = "none";
+  }
+  else{
+      dots.style.display = "none";
+      btn.innerHTML = "Read less";
+      moretext.style.display = "inline";
+  }
+}
+function readmore7(){
+  var dots= document.getElementById("Dots7");
+  var moretext= document.getElementById("more7");
+  var btn= document.getElementById("btn7");
+
+  if(dots.style.display == "none"){
+      dots.style.display== "inline";
+      btn.innerHTML = "Read more";
+      moretext.style.display = "none";
+  }
+  else{
+      dots.style.display = "none";
+      btn.innerHTML = "Read less";
+      moretext.style.display = "inline";
+  }
+}
+function readmore8(){
+  var dots= document.getElementById("Dots8");
+  var moretext= document.getElementById("more8");
+  var btn= document.getElementById("btn8");
+
+  if(dots.style.display == "none"){
+      dots.style.display== "inline";
+      btn.innerHTML = "Read more";
+      moretext.style.display = "none";
+  }
+  else{
+      dots.style.display = "none";
+      btn.innerHTML = "Read less";
+      moretext.style.display = "inline";
+  }
+}
+function readmore9(){
+  var dots= document.getElementById("Dots9");
+  var moretext= document.getElementById("more9");
+  var btn= document.getElementById("btn9");
+
+  if(dots.style.display == "none"){
+      dots.style.display== "inline";
+      btn.innerHTML = "Read more";
+      moretext.style.display = "none";
+  }
+  else{
+      dots.style.display = "none";
+      btn.innerHTML = "Read less";
+      moretext.style.display = "inline";
+  }
+}
